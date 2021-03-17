0b1510722831fe78b4d3302b25a7372f05acf81b
Pyzyryab <pyzyryab@tutanota.com>
2021-03-17 15:49:59 +0100

    Dynamically way to add path on list files/directories

--- src/linux/directories.py
+++ src/linux/directories.py
@@ -1,8 +1,9 @@
 import subprocess
 class Directories:
     
-    def __init__(self, action):
+    def __init__(self, action, path='/', *args, **kwargs):
         self.action = action
+        self.path = path
         self.args = []
         self.set_args()
 
@@ -19,9 +20,12 @@ class Directories:
     def set_args(self):
         action_switcher = {
             1 : ['ls', '-l'],
-            2 : ['ls', '-la']
+            2 : ['ls', '-la'],
         }
+
         self.args = [command for command in action_switcher.get(self.action)]
+        if self.path != '':
+            self.args.append(self.path)
         print(f'self.args -> {self.args}')
         return self.args
 
